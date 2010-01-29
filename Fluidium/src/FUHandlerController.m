0356f450d6df00862c589065394042eced2846cf
Todd Ditchendorf <todd.ditchendorf@gmail.com>
2010-01-29 00:32:38 -0800

    handlers

--- Fluidium/src/FUHandlerController.m
+++ Fluidium/src/FUHandlerController.m
@@ -20,6 +20,10 @@
 #define KEY_SCHEME @"scheme"
 #define KEY_URLSTRING @"URLString"
 
+@interface FUHandlerController ()
+- (void)getEmailAddr:(NSString **)emailAddr args:(NSMutableDictionary **)args fromMailToURL:(NSString *)URLString;
+@end
+
 @implementation FUHandlerController
 
 + (FUHandlerController *)instance {
@@ -79,13 +83,73 @@
     
     NSDictionary *handler = [lookupTable objectForKey:scheme];
     if (handler) {
+        NSString *replacement = [handler objectForKey:KEY_URLSTRING];
         NSString *inURLString = [[[req URL] absoluteString] substringFromIndex:[scheme length] + 1];
-        NSString *replacement = [[handler objectForKey:KEY_URLSTRING] stringByAppendingString:inURLString];
+        
+        // custom logic for mailto (it's common)
+        if ([scheme isEqualToString:@"mailto"]) {
+            NSString *emailAddr = nil;
+            NSMutableDictionary *args = [NSMutableDictionary dictionary];
+            [self getEmailAddr:&emailAddr args:&args fromMailToURL:inURLString];
+            if (emailAddr) {
+                replacement = [replacement stringByAppendingString:emailAddr];
+            }
+            for (NSString *name in args) {
+                NSString *value = [args objectForKey:name];
+                [replacement stringByAppendingFormat:@"&%@=%@", name, value];
+            }
+        } else {
+            replacement = [replacement stringByAppendingString:inURLString];
+        }
         replacement = [replacement stringByEnsuringURLSchemePrefix];
         return [NSURLRequest requestWithURL:[NSURL URLWithString:replacement]];
     } else {
         return req;
     }
+}
+
+
+- (void)getEmailAddr:(NSString **)emailAddr args:(NSMutableDictionary **)args fromMailToURL:(NSString *)URLString {
+	@try {
+		NSMutableString *mstr = [[URLString mutableCopy] autorelease];
+		[mstr replaceOccurrencesOfString:@"&amp;"
+							  withString:@"&"
+								 options:0
+								   range:NSMakeRange(0, [mstr length])];
+		
+		NSString *argStr = @"";
+		NSArray *splits = nil;
+		
+		if ([@"?" isEqualToString:[mstr substringWithRange:NSMakeRange(0, 1)]]) {
+			argStr = [mstr substringWithRange:NSMakeRange(1, [mstr length] - 1)];
+		} else if (NSNotFound == [mstr rangeOfString:@"?"].location) {
+            if (emailAddr) *emailAddr = [NSString stringWithString:mstr];
+		} else {
+			splits = [mstr componentsSeparatedByString:@"?"];
+			if (emailAddr) *emailAddr = [splits objectAtIndex:0];
+			argStr = [splits objectAtIndex:1];
+		}
+		
+		//NSLog(@"emailAddr: %@, argStr: %@", (*emailAddr), argStr);
+		
+		splits = [argStr componentsSeparatedByString:@"&"];
+		
+        if (args) {
+            for (NSString *arg in splits) {
+                NSArray *a = [arg componentsSeparatedByString:@"="];
+                if (2 == [a count]) {
+                    NSString *value = [[a objectAtIndex:1] stringByReplacingPercentEscapesUsingEncoding:NSUTF8StringEncoding];
+                    [(*args) setObject:value forKey:[a objectAtIndex:0]];
+                }
+            }
+        }
+		
+		//NSLog(@"emailAddr: %@, args: %@", (*emailAddr), (*args));
+        
+	} @catch (NSException *e) {
+		if (emailAddr) *emailAddr = @"";
+		if (args) *args = nil;
+	}
 }
 
 @synthesize handlers;
